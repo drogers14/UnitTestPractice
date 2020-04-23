370476d3d7c40d12b93c5840c98c9571831e3a91
Destiny Rogers <destinyrogers725@gmail.com>
2020-04-23 11:10:15 -0700

    fix

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -1,45 +1,47 @@
 /**
  * Unit Tests for Practice class
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Practice.h"
 
 class PracticeTest : public ::testing::Test
 {
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+  protected:
+    PracticeTest(){} //constructor runs before each test
+    virtual ~PracticeTest(){} //destructor cleans up after tests
+    virtual void SetUp(){} //sets up before each test (after constructor)
+    virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(PracticeTest, is_simple_palindrome)
 {
-    Practice obj;
-    bool actual = obj.isPalindrome("aa");
-    ASSERT_TRUE(actual);
+  Practice obj;
+  bool actual = obj.isPalindrome("aa");
+  ASSERT_TRUE(actual);
 }
 
 TEST(PracticeTest, is_not_palindrome)
 {
-    Practice obj;
-    bool actual = obj.isPalindrome("Ba");
-    ASSERT_FALSE(actual);
+  Practice obj;
+  bool actual = obj.isPalindrome("Ba");
+  ASSERT_FALSE(actual);
 }
 
 TEST(PracticeTest, is_a_palindrome)
 {
-    Practice obj;
-    bool actual = obj.isPalindrome("#$#");
-    ASSERT_TRUE(actual);
+  Practice obj;
+  bool actual = obj.isPalindrome("#$#");
+  ASSERT_TRUE(actual);
 }
 
 TEST(PracticeTest, is_ascending)
 {
-    Practice obj;
-    int arr[3] = {2, 3, 4};
-    int actual[3] = obj.sortDescending(arr);
-    int check[3] = {4, 3, 2};
-    EXPECT_EQ(check, actual);
+  Practice obj;
+  int one = 4;
+  int two = 2;
+  int three = 3;
+
+  obj.sortDescending(one,two,three );
+  EXPECT_LT(one, two);
 }
