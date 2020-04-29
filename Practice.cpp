49f65db34650db67c592a8fabcc0ef45fbe7b82e
Destiny Rogers <destinyrogers725@gmail.com>
2020-04-29 12:24:33 -0700

    typooooo

--- Practice.cpp
+++ Practice.cpp
@@ -32,7 +32,7 @@ void Practice::sortDescending(int & first, int & second, int & third)
 // character in the string, but disregarding case ('x' is the same as 'X')
 bool Practice::isPalindrome(string input)
 {
-  if(input.size == 1 || input.size() == 0){
+  if(input.size() == 1 || input.size() == 0){
     return true;
   }
 
